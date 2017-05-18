1716500212ccc973fc8a3155bcc6ed1be4f10fb0
Sokureo <mancunianway@mail.ru>
2017-05-18 23:21:51 +0300

    np

--- PoS/hittite_pos_tagger.py
+++ PoS/hittite_pos_tagger.py
@@ -1,22 +1,21 @@
-from bs4 import BeautifulSoup
 import os
 import re
 import numpy as np
-import closed_class
 import train_set
 import word_order
 
 
 def pos():
-    closed = closed_class.Closed()  # гениально
 
-    return closed
+    #word_order.word_order() # проставляем порядковый номер слова в обучающем материале
+    train_features_set, train_tag_list = train_set.make_train_set() # делаем обучающую матрицу
+
+
+
+
 
 
 def main():
-    word_order.word_order() # проставляем порядковый номер слова в обучающем материале
-    train_matrix = train_set.make_train_set() # делаем обучающую матрицу
-
     pos()
 
 
--- PoS/train_set.py
+++ PoS/train_set.py
@@ -1,11 +1,12 @@
 from bs4 import BeautifulSoup
 import os
 import re
+import numpy as np
 import closed_class
+
 
 def make_train_set(): # делаем обучающий сет
 
-    train_matrix = []
     closed = closed_class.Closed()
     # считываем класс закрытых классов
     speach_class = {i.split()[1]: i.split()[0] for i in open('../class.txt').read().split('\n') if i != ''}
@@ -13,12 +14,16 @@ def make_train_set(): # делаем обучающий сет
     features = open('../features.txt').read().split()
     # считываем признаки
 
+    train_word_list = []  # массив для обучающих слов
+    train_features_set = []  # массив для обучающих признаков
+    train_tag_list = []  # массив с правильными частями речи
+
     fname_list = os.listdir('../materials_xlsx/') # массив имён файлов с обучающими материалами в .xlsx
 
     for fname in fname_list: # для каждого файла
 
         print('processing ' + fname + '\n')
-        os.system('libreoffice --convert-to xml --outdir ./materials_xml/ ../materials_xlsx/' + fname) # конвертируем в .xml
+        #os.system('libreoffice --convert-to xml --outdir ./materials_xml/ ../materials_xlsx/' + fname) # конвертируем в .xml
         # результат положится в папку materials_xml в данной дериктории
 
         soup = BeautifulSoup(open('./materials_xml/' + fname.replace('xlsx', 'xml')), 'xml') # читаем дерево
@@ -36,18 +41,21 @@ def make_train_set(): # делаем обучающий сет
             if row[0] != '':
                 if len(row) > 8 and row[8].strip() in speach_class.keys():
                     example = []
-                    example.append(row[5].strip('!: '))
-                    for feat in features:
+                    train_word_list.append(row[5].strip('!: ')) # добавляем слово
+                    for feat in features: # добавляем его признаки
                         if row[5].strip('!: ').endswith(feat):
                             example.append(1) # есть признак
                         else:
                             example.append(0) # нет признака
-                    example.append(row[10])
-                    example.append(speach_class[row[8].strip()]) # порядковый номер части речи
-                    train_matrix.append(example) # получаем матрицу из примеров
+                    example.append(row[10]) # добавляем номер в предложении
+                    train_tag_list.append(speach_class[row[8].strip()]) # порядковый номер правильной части речи
+                    train_features_set.append(example) # получаем матрицу из примеров
+
+    train_features_set = np.array(train_features_set)
+    train_tag_list = np.array(train_tag_list)
 
     print('train_matrix made')
 
-    return train_matrix
+    return train_features_set, train_tag_list
 
 
